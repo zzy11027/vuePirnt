b68351048351a3f4ff1b6300f8ab40215210385c
zhangziyang <15033572695@163.com>
2022-02-21 00:33:27 +0800

    feat:测试部署

--- src/index.js
+++ src/index.js
@@ -1,27 +1,25 @@
-import KrPrintDesigner from './App.vue'
-import ElementUI from 'element-ui';
+import KrPrintDesigner from "./App.vue";
+import ElementUI from "element-ui";
 
-import 'element-ui/lib/theme-chalk/index.css';
-import './assets/style/index.scss'
+import "element-ui/lib/theme-chalk/index.css";
+import "./assets/style/index.scss";
 import "./assets/style/fontIcon.css";
 import "./assets/style/fontIcon.js";
 // LODOP打印服务
-import './libs/lodop/LodopFuncs.js'
-import lodop from './libs/lodop/index.js'
+import "./libs/lodop/LodopFuncs.js";
+import lodop from "./libs/lodop/index.js";
 
 const install = function (Vue, opts = {}) {
   Vue.use(ElementUI);
-  Vue.component('kr-print-designer', KrPrintDesigner)
-  Vue.prototype.$lodop = lodop
-
+  Vue.component("kr-print-designer", KrPrintDesigner);
+  Vue.prototype.$lodop = lodop;
 };
 
-if (typeof window !== 'undefined' && window.Vue) {
+if (typeof window !== "undefined" && window.Vue) {
   install(window.Vue);
 }
 
-
 export default {
   install,
-  KrPrintDesigner
-}
+  KrPrintDesigner,
+};
